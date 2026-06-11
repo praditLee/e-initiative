60b48b2f38258c0bc3a236621e0f72f731bdede7
manncomely <pradit.leela@gmail.com>
2026-06-11 19:29:53 +0700

    ใส่ subdomain ใน Meta

--- e-initiative.config.mjs
+++ e-initiative.config.mjs
@@ -3,8 +3,8 @@ export default {
 	metadata: {
 		title: 'ANTS - ร่างรัฐธรรมนูญประชาชน',
 		description: 'กองทัพมดเดินหน้าร่างรัฐธรรมนูญประชาชน',
-		siteUrl: 'https://e-initiative.pages.dev',
-		previewImageUrl: 'https://e-initiative.pages.dev/og.png',
+		siteUrl: 'https://action.actlabthailand.org',
+		previewImageUrl: 'https://action.actlabthailand.org/og.png',
 	},
 	petition: {
 		endDate: new Date('2026-06-30 GMT+7'),
