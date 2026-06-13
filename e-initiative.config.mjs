2748ce15f7da81033d993d75dc4383c307f598da
manncomely <pradit.leela@gmail.com>
2026-06-13 15:14:30 +0700

    แก้ตารางและคำประกาศ

--- e-initiative.config.mjs
+++ e-initiative.config.mjs
@@ -19,7 +19,7 @@ export default {
 					'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3873.298251120154!2d100.56221937605227!3d13.881106394284691!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e28323d10290b5%3A0x946e22bc8754445a!2sOffice%20of%20Election%20Commission%20of%20Thailand!5e0!3m2!1sen!2sth!4v1716288187164!5m2!1sen!2sth',
 			},
 			volunteer: {
-				registrationFormUrl: '#volunteer-register',
+				registrationFormUrl: 'https://docs.google.com/forms/d/e/1FAIpQLScFLIZOa5n2QVmK-hivfXH9e8FL82XtfDznna_LZpliL8sNgw/viewform',
 				assetsUrl: '/share/1',
 				contact: {
 					name: 'ActLab Thailand',
